126916eb72e35dbf077c070d0d577de87a27bf83
Chris Kang <kangthchris@gmail.com>
2024-07-16 15:44:47 +1000

    Taking way too much time setting up imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './components/App/App';
+import App from 'components/App/App';
+import { createTheme, MantineProvider } from '@mantine/core';
+const theme = createTheme({
+    fontFamily: 'Open Sans, sans-serif',
+    primaryColor: 'cyan',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
+);
